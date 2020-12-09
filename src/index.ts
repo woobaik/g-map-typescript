1f8b110aa7e712887957e565a1a317e2d7417c3b
Jbaik <joungwoo.baik@gmail.com>
2020-12-09 11:15:02 -0800

    finish working around google map api

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+import {User} from './User'
+import {Company} from './Company'
+
+console.log(new User())
+console.log(new Company())
+
+new google.maps.Map(document.getElementById('map'), {zoom: 1, center: {lat: 0, lng:0}})
